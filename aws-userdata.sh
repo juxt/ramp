3b5c6ac242708c67422863674f2e1a74b39e08c8
Roxolan0 <hugo@juxt.pro>
2018-09-18 16:15:42 +0100

    Make template and userdata not run a simulation

--- aws-userdata.sh
+++ aws-userdata.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-TargetUrl=$1
-PeakUsers=$2
-Duration=$3
-BucketName=$4
-SelfDestruct=$5
-StackName=$6
-Region=$7
-
 yum remove -y java-1.7.0-openjdk
 yum install -y java-1.8.0
 cd /
@@ -18,16 +10,3 @@ mv gatling-charts-highcharts-bundle-2.3.1/ gatling/
 cd gatling/
 rm -rf user-files/simulations/
 mkdir user-files/simulations/
-wget -P /gatling/user-files/simulations/ http://$BucketName.s3.amazonaws.com/LoadSimulation.scala
-
-echo "PeakUsers=$PeakUsers
-Duration=$Duration
-TargetUrl=$TargetUrl" > results/params.txt
-
-JAVA_OPTS="-DPeakUsers=$PeakUsers -DDuration=$Duration -DTargetUrl=$TargetUrl" ./bin/gatling.sh -m > results/gatling.out
-
-aws s3 cp --recursive results/ s3://$BucketName/
-
-if [ $SelfDestruct == true ]; then
-    aws cloudformation delete-stack --stack-name $StackName --region $Region
-fi
